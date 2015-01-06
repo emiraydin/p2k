10bcbf90c2c865f2902c18da6d0f87b02658baf8
Emir Aydin <emraydn12@gmail.com>
2015-01-06 02:13:26 -0500

    Remove cronjob from bin
    It's already in the README

--- bin/cronjob.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add the kindlegen and kindlerb to PATH
-export PATH=$PATH:/home/username/app/vendor/bin:/usr/local/bin
-
-# Run the delivery processor
-cd /home/username/app && bin/rails runner -e production "DeliveryProcessor.check"
