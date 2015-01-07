396e8c5a5e543eef0dd132333f05810fe9d92cbb
Emir Aydin <emraydn12@gmail.com>
2015-01-07 18:41:29 -0500

    Fix the access token update bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -16,11 +16,20 @@ class WelcomeController < ApplicationController
 	session[:access_token] = response['access_token']
 
 	user = User.where(:username => session[:username]).first
+
+	# If the user is not new and has a delivery set up, update their access token
+	# And redirect them to "Manage deliveries" page
 	if (not user.nil? and not user.delivery.nil?)
+		user.access_token = session[:access_token]
+		user.save
 		redirect_to "/manage/home"
 	else
+		# If the user is new, create an account, otherwise just update the access token
 		if (user.nil?)
 			User.create(:username => session[:username], :access_token => session[:access_token])
+		else
+			user.access_token = session[:access_token]
+			user.save
 		end
 		redirect_to "/deliveries/home"
 	end
