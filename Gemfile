3dbc44d0a7aa80ff1be290f96e1d99cd839c8c81
Emir Aydin <emir@emiraydin.com>
2015-10-19 02:50:26 -0700

    fix(global): conditional config loading for tests

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
-# gem 'sqlite3'
+gem 'sqlite3', group: :test
 gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails'
--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,9 @@
 class Settings < Settingslogic
-	source "#{Rails.root}/config/application.yml"
-	namespace Rails.env
+	if not Rails.env.test?
+		source "#{Rails.root}/config/application.yml"
+		namespace Rails.env
+	else
+		source "#{Rails.root}/config/application_test.yml"
+		namespace Rails.env
+	end
 end
